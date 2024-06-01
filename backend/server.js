d7d0a0d9132c394d789abf1dc67699c8ae00f010
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-06-01 16:57:42 +0530

    backend

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,13 @@
 const express = require('express')
 
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.json({
+        msg:"FUck"
+    });
+})
+
+app.listen(5000, ()=>{
+    console.log('Listening to 5000');
+})
