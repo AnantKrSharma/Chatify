fe3150ca40a26e27250d0e41002e7ee204e09aa9
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-06-02 16:28:09 +0530

    signup endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,17 +1,54 @@
+import User from '../models/user.model.js'
 
 export async function signup(req, res){
     try{
-        const {yo} = await req.body;
+        const {fullName, username, password, confirmPassword, gender} = req.body;
+
+        if(password !== confirmPassword){
+            res.status(400).json({
+                errors: "Passwords don't match."
+            })
+        }
+
+        const found = await User.findOne({username});
+
+        if(found){
+            return res.status(400).json({
+                errors: "User already exists."
+            })
+        }
+        
+        // HASH PASSWORD HERE
+
+        // https://avatar-placeholder.iran.liara.run/
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
+        const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
+
+        const created = await User.create({
+            fullName,
+            username,
+            password, 
+            gender: gender.toLowerCase(),
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic
+        })
+
+        res.status(201).json({
+            _id: created._id,
+            fullName: created.fullName,
+            username: created.username,
+            password: created.password,
+            gender: created.gender,
+            profilePic: created.profilePic 
+        })
+    } 
     
-        res.json({
-            msg: `${yo}UUUU`
-        })
-    }
     catch(error){
-        res.json({
-            msg:"Sorry, fucked."
+        console.log("Error in signup controller", error.message);
+
+        res.status(500).json({
+            errors: "Error while creating new user."
         })
-        console.log(error);
     }
 }
 
