c9831a9e66efd4b03d5d122414916cfe4ae239cc
Anant Kr Sharma <anantkrsharma01@gmail.com>
2024-06-02 20:06:31 +0530

    Jwt-Token and Signup Endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,17 +1,18 @@
 import User from '../models/user.model.js'
+import bcryptjs from 'bcryptjs'
+import generateTokenAndSetCookie from '../utils/generateToken.js';
 
 export async function signup(req, res){
     try{
         const {fullName, username, password, confirmPassword, gender} = req.body;
 
         if(password !== confirmPassword){
-            res.status(400).json({
+            return res.status(400).json({
                 errors: "Passwords don't match."
             })
         }
 
         const found = await User.findOne({username});
-
         if(found){
             return res.status(400).json({
                 errors: "User already exists."
@@ -19,32 +20,32 @@ export async function signup(req, res){
         }
         
         // HASH PASSWORD HERE
+        const salt = await bcryptjs.genSalt(10);
+        const hashedPassword = await bcryptjs.hash(password, salt);
 
         // https://avatar-placeholder.iran.liara.run/
-
         const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`
         const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`
 
         const created = await User.create({
             fullName,
             username,
-            password, 
+            password: hashedPassword, 
             gender: gender.toLowerCase(),
             profilePic: gender === "male" ? boyProfilePic : girlProfilePic
         })
+
+        generateTokenAndSetCookie(created._id, res);
 
         res.status(201).json({
             _id: created._id,
             fullName: created.fullName,
             username: created.username,
-            password: created.password,
-            gender: created.gender,
             profilePic: created.profilePic 
         })
-    } 
-    
+    }    
     catch(error){
-        console.log("Error in signup controller", error.message);
+        console.log("Error in signup controller - ", error.message);
 
         res.status(500).json({
             errors: "Error while creating new user."
@@ -53,11 +54,14 @@ export async function signup(req, res){
 }
 
 
+
 export async function login(req, res){
     res.json({
         msg: "YO LOGIN ?" 
     })
 }
+
+
 
 export async function logout(req, res){
     res.json({
